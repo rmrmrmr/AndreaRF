6dfd7daf29902dab82bbaae9abc335ccfef70a6d
rmrmrmr <ricardomartineza77@gmail.com>
2023-06-02 02:03:21 -0600

    Add under-construction sign to portfolio sign to portfolio section

--- src/app/(components)/Portfolio.tsx
+++ src/app/(components)/Portfolio.tsx
@@ -2,6 +2,9 @@ export default function Portfolio() {
   return (
     <section className="w-full h-screen flex flex-col justify-start items-center py-5 px-5 gap-4" id="work">
       <h2 className="text-5xl font-bold mb-2">Work</h2>
+      <div className="flex flex-col items-center justify-center grow">
+        <p className="text-xl font-medium">Section under construction 👷🏻‍♀️🏗️</p>
+      </div>
     </section>
   )
 }
